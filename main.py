cab03795071d14907c8bd747d6ec8b16b473aabb
SnipperPlayer <galeana2002@hotmail.com>
2021-05-06 10:49:56 -0500

    color

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def line(start, end):
     goto(start.x, start.y)
     down()
     goto(end.x, end.y)
+    
 
 def square(start, end):
     "Draw square from start to end."
@@ -26,7 +27,7 @@ def square(start, end):
     goto(start.x, start.y)
     down()
     begin_fill()
-
+    
     for count in range(4):
         forward(end.x - start.x)
         left(90)
@@ -35,7 +36,16 @@ def square(start, end):
 
 def circle(start, end):
     "Draw circle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    
+    for count in range(20):
+        forward(end.x - start.x)
+        left(20)
+
+    end_fill()
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
@@ -71,6 +81,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('yellow'), 'Y')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
